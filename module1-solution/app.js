84ecfa1e76ae3517de151a0e47b2829b1309eff9
Dragan Savevski <dragansavevski@gmai.com>
2017-01-05 20:20:06 +0100

    assignment done

---
+++ module1-solution/app.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict'
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+
+
+        $scope.check = function() {
+            if ($scope.food != undefined && $scope.food != "") {
+                var parts = $scope.food.split(',');
+                var cnt = 0;
+
+                for (var tmp in parts) {
+                    //console.log(parts[tmp]);
+                    if (parts[tmp].length > 1) { //checking for an empty string
+                        cnt++;
+                    }
+                }
+
+                if (cnt > 3) {
+                    $scope.message = 'Too much!';
+                } else {
+                    $scope.message = 'Enjoy!';
+                }
+                $scope.color = 'success';
+                $scope.state = 'success';
+
+            } else {
+                $scope.message = 'Please enter data first';
+                $scope.color = 'danger';
+                $scope.state = 'error';
+            }
+        }
+    }
+
+
+
+})();
